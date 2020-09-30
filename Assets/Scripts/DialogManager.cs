f4e3d9ea09a4da54f1e09438e5d9e30b9e4357ab
TaaviM88Laptop <juomakuski@hotmail.com>
2020-09-30 13:23:29 +0300

    dialog aloitettu

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class DialogManager : MonoBehaviour
+{
+    public TMP_Text dialogText;
+    public TMP_Text nameText;
+    public GameObject dialogBox;
+    public GameObject nameBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
